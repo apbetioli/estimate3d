3507371a997c56fa4df861e9b673b7c2c0720d80
hasibot <hasitha.developer@gmail.com>
2020-12-26 14:40:41 +0530

    Process new charges values and pass to the result page  - calculate sumTotalCharge  - calculate FilamentCharge  - calculate EnergyCharge  - calculate PrintTimeCharge

--- src/main/java/owlracle3d/estimator/controller/EstimatorViewController.java
+++ src/main/java/owlracle3d/estimator/controller/EstimatorViewController.java
@@ -71,7 +71,7 @@ public class EstimatorViewController {
             @RequestPart("filament_charge") String filament_charge,
             @RequestPart("energy_charge") String energy_charge,
             @RequestPart("print_time_charge") String print_time_charge,
-            @RequestPart("additional_chargee") String additional_charge,
+            @RequestPart("additional_charge") String additional_charge,
 
             Map<String, Object> model)
             throws IOException, InterruptedException, ArchiveException {
@@ -101,7 +101,12 @@ public class EstimatorViewController {
                 work_hours,
 
                 profit,
-                transaction_fee
+                transaction_fee,
+
+                filament_charge,
+                energy_charge,
+                print_time_charge,
+                additional_charge
         );
 
         model.put("estimative", estimative);
