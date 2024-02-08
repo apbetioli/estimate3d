3e00b9ab7e82b34d90427477955024e24c2e9117
Alexandre Betioli <apbetioli@gmail.com>
2024-02-08 20:10:12 +0100

    Refactor App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { class: "text-xl" }, "Hello World"),
-    React.createElement("p", {}, "This is a paragraph"),
-  ]);
-};
+const App = () => (
+  <div>
+    <h1 className="text-xl">Hello</h1>
+  </div>
+);
 
 const container = document.getElementById("root");
 const root = createRoot(container as Element);
-root.render(React.createElement(App));
+root.render(<App />);
