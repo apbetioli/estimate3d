5dd3f0d503e5949d8e4971e095cc120129138244
Alexandre Betioli <apbetioli@gmail.com>
2024-02-23 10:09:02 +0100

    Add 'eslint-plugin-tailwindcss' plugin

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -18,7 +18,7 @@ const Tab = ({ to, children }: PropsWithChildren<TabProps>) => {
         "inline-flex h-12 items-center whitespace-nowrap border-gray-300 px-4 py-2 text-center  text-sm text-gray-700 focus:outline-none dark:border-gray-500 dark:text-white sm:text-base",
         {
           "rounded-t-md border border-b-0": active,
-          "cursor-base border-b bg-transparent hover:border-gray-400 dark:hover:border-gray-300":
+          "border-b bg-transparent hover:border-gray-400 dark:hover:border-gray-300":
             !active,
         },
       )}
