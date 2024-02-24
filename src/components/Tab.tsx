be1616621b263b1109004e2d4964d786c16fb78b
Davide Coppola <davide.coppola97@gmail.com>
2024-02-24 13:55:42 +0100

    Format fix

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -17,7 +17,7 @@ const Tab = ({ to, onClick, children }: PropsWithChildren<TabProps>) => {
       to={to}
       onClick={onClick}
       className={classnames(
-        "px-12 md:px-4 items-center whitespace-nowrap py-3 text-center text-sm text-gray-700 focus:outline-none dark:border-gray-500 dark:text-white sm:text-base md:border-gray-300",
+        "items-center whitespace-nowrap px-12 py-3 text-center text-sm text-gray-700 focus:outline-none dark:border-gray-500 dark:text-white sm:text-base md:border-gray-300 md:px-4",
         {
           "rounded-md font-bold md:rounded-none md:rounded-t-md md:border md:border-b-0 md:bg-transparent md:font-normal":
             active,
