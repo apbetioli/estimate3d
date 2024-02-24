19711a1cb38ea4980fa95280430dd33ab48d51f1
Davide Coppola <davide.coppola97@gmail.com>
2024-02-24 13:53:04 +0100

    Expose onClick rather than setShowMenu and fix padding

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -5,19 +5,19 @@ import classnames from "classnames";
 
 type TabProps = {
   to: To;
-  setShowMenu: (value: boolean) => void;
+  onClick?: () => void;
 };
 
-const Tab = ({ to, children, setShowMenu }: PropsWithChildren<TabProps>) => {
+const Tab = ({ to, onClick, children }: PropsWithChildren<TabProps>) => {
   const { pathname } = useLocation();
   const active =
     pathname == to || (String(to) !== "/" && pathname.startsWith(String(to)));
   return (
     <Link
-      onClick={() => setShowMenu(false)}
       to={to}
+      onClick={onClick}
       className={classnames(
-        "inline-flex h-12 items-center whitespace-nowrap px-4 py-2 text-center text-sm text-gray-700 focus:outline-none dark:border-gray-500 dark:text-white sm:text-base md:border-gray-300",
+        "px-12 md:px-4 items-center whitespace-nowrap py-3 text-center text-sm text-gray-700 focus:outline-none dark:border-gray-500 dark:text-white sm:text-base md:border-gray-300",
         {
           "rounded-md font-bold md:rounded-none md:rounded-t-md md:border md:border-b-0 md:bg-transparent md:font-normal":
             active,
