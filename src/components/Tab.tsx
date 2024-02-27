9e34a4eac6365680e28fb3dbca1df39d27c96cef
Davide Coppola <95505688+dvdcpp@users.noreply.github.com>
2024-02-27 17:11:02 +0100

    Format fix

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -19,8 +19,10 @@ const Tab = ({ to, onClick, children }: PropsWithChildren<TabProps>) => {
       className={classnames(
         "items-center whitespace-nowrap px-12 py-3 text-center text-sm text-gray-700 focus:outline-none dark:border-gray-500 dark:text-white sm:text-base md:border-gray-300 md:px-4",
         {
-          "rounded-md font-bold md:rounded-none md:rounded-t-md md:border md:border-b-0 md:bg-transparent md:font-normal": active,
-          "cursor-base bg-transparent hover:border-gray-400 dark:hover:border-gray-300 md:border-b": !active,
+          "rounded-md font-bold md:rounded-none md:rounded-t-md md:border md:border-b-0 md:bg-transparent md:font-normal":
+            active,
+          "cursor-base bg-transparent hover:border-gray-400 dark:hover:border-gray-300 md:border-b":
+            !active,
         },
       )}
     >
