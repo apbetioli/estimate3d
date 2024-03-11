0bfa368a44733160a5275aa0ef798fea748d11d4
Anukrati Mehta <anukratimehta@gmail.com>
2024-03-11 13:35:29 +0530

    Refactored SVG Icons (#40)

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,6 @@
 import Tab from "./Tab";
 import { useState, useEffect } from "react";
+import { MenuIcon } from "./Icons";
 
 const Menu = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -33,22 +34,7 @@ const Menu = () => {
         className="btn-secondary ml-2 block rounded-lg border p-2 md:hidden"
         onClick={() => setShowMenu(!showMenu)}
       >
-        <svg
-          fill="currentColor"
-          height="25"
-          width="25"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 32.055 32.055"
-        >
-          <g>
-            <path
-              d="M3.968,12.061C1.775,12.061,0,13.835,0,16.027c0,2.192,1.773,3.967,3.968,3.967c2.189,0,3.966-1.772,3.966-3.967
-              C7.934,13.835,6.157,12.061,3.968,12.061z M16.233,12.061c-2.188,0-3.968,1.773-3.968,3.965c0,2.192,1.778,3.967,3.968,3.967
-              s3.97-1.772,3.97-3.967C20.201,13.835,18.423,12.061,16.233,12.061z M28.09,12.061c-2.192,0-3.969,1.774-3.969,3.967
-              c0,2.19,1.774,3.965,3.969,3.965c2.188,0,3.965-1.772,3.965-3.965S30.278,12.061,28.09,12.061z"
-            />
-          </g>
-        </svg>
+        <MenuIcon />
       </button>
       <div
         className={`${showMenu ? "" : "hidden"} absolute right-0 top-full z-[1] mt-2 rounded-lg bg-white text-center shadow-xl dark:bg-slate-700 md:static md:block md:bg-transparent md:px-0 md:shadow-none md:dark:bg-transparent`}
--- src/pages/Prints.tsx
+++ src/pages/Prints.tsx
@@ -8,6 +8,8 @@ import Section from "../components/Section";
 import { useState } from "react";
 import ConfirmationDialog from "../components/ConfirmationDialog";
 
+import { AddIcon, TrashIcon, PenIcon } from "../components/Icons";
+
 const Prints = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [deletingPrint, setDeletingPrint] = useState<Print>();
@@ -30,20 +32,7 @@ const Prints = () => {
       <div className="my-5 flex h-[40px] w-full items-center justify-between rounded-lg bg-gray-100 pl-4 dark:bg-gray-800">
         <Breadcrumb pages={[{ name: "Prints" }]} />
         <Link className="btn btn-primary whitespace-nowrap" to="/prints/new">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
+          <AddIcon />
           Add
         </Link>
       </div>
@@ -120,20 +109,7 @@ const Prints = () => {
                           setDeletingPrint(p);
                         }}
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="h-5 w-5"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                          />
-                        </svg>
+                        <TrashIcon />
                         <span className="sr-only">Remove</span>
                       </button>
 
@@ -141,20 +117,7 @@ const Prints = () => {
                         className="text-gray-500 transition-colors duration-200 hover:text-yellow-500 focus:outline-none dark:text-gray-300 dark:hover:text-yellow-500"
                         to={`/prints/${p.id}`}
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="h-5 w-5"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                          />
-                        </svg>
+                        <PenIcon />
                         <span className="sr-only">Edit</span>
                       </Link>
                     </div>
